01f3b8d0407f2f835bf0044374d43c6ca546eba2
tsuyohase <tsuyohase123@gmail.com>
2023-06-14 20:34:24 +0900

    httpリクエストコミット忘れ

--- communication/server.js
+++ communication/server.js
@@ -12,6 +12,9 @@ expressWs(app);
 const websocketRouter = require("./websocket");
 app.use("/websocket", websocketRouter);
 
+const httpRouter = require("./http");
+app.use("/http", httpRouter);
+
 app.get("*", (req, res) => {
   res.sendStatus(404);
 });
