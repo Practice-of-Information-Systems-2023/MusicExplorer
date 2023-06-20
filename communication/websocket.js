657e781a5788f7a74b2fd33addec391d8ffb8ee0
tsuyohase <tsuyohase123@gmail.com>
2023-06-20 15:19:50 +0900

    connect websocket

--- communication/websocket.js
+++ communication/websocket.js
@@ -9,10 +9,10 @@ const { v4: uuidv4 } = require("uuid");
 const clients = new Map();
 
 router.ws("/test", (ws, req) => {
-  id = uuidv4();
-  console.log("新しいクライアント: " + id);
-  clients.set(id, ws);
-  ws.send(id);
+  let identify;
+  console.log("新しいクライアント");
+
+  // ws.send(id);
 
   let interval;
   interval = setInterval(() => {
@@ -24,19 +24,20 @@ router.ws("/test", (ws, req) => {
   }, 100);
 
   ws.on("message", (msg) => {
-    // ws.send(msg);
-    // console.log(msg);
     const data = JSON.parse(msg);
-    const { id, username, x, y, field } = data;
+    const { id, x, y } = data;
 
-    addUserData(id, username, x, y, field);
+    identify = id;
+    clients.set(identify, ws);
+
+    addUserData(identify, x, y);
     // const response = getUserData();
     // ws.send(JSON.stringify(response));
   });
 
   ws.on("close", () => {
-    clients.delete(id);
-    deleteUserData(id);
+    clients.delete(identify);
+    deleteUserData(identify);
   });
 });
 
