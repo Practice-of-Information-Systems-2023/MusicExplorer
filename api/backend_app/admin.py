b3d248ef64474ef1309bb621685f2ebe2ff054a6
yutorse <yutohorse.coc@gmail.com>
2023-06-26 19:27:27 +0900

    gitignore追記

--- api/backend_app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from . models import AppUser,Genre,Favorite,Music
-
-admin.site.register(AppUser)
-admin.site.register(Genre)
-admin.site.register(Favorite)
-admin.site.register(Music)
